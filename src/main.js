4f53eeaaac11a47cb320cd8d1735e7e76ec5f303
hailinyz <1660246929@qq.com>
2026-01-02 18:07:19 +0800

    B端前端

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import "@/assets/main.scss"
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import './assets/main.scss'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
-
 app.use(ElementPlus, {
   locale: zhCn,
 })
+app.mount('#app')
 
